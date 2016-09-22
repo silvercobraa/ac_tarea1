6d428a5650b646002724718966119a9ff528ca5e
silvercobraa <silver.cobra1n@gmail.com>
2016-09-22 20:32:37 -0300

    Se corrigió isPositive y se agregó isGreater

--- tarea1.c
+++ tarea1.c
@@ -25,8 +25,11 @@ int main(int argc, char const *argv[])
     printf("%d\n", isPositive(5));
     printf("%d\n", isPositive(0));
     printf("%d\n", isPositive(-1));
-    printf("%d\n", isPositive(-25434));
-    printf("%d\n", isPositive(3453));
+    printf("isPositive %d: %d\n", -25434, isPositive(-25434));
+    printf("isPositive %d: %d\n", 3453, isPositive(3453));
+    printf("isPositive %d: %d\n", 2, isPositive(2));
+    printf("isPositive %d: %d\n", -2, isPositive(-2));
+    printf("isPositive %d: %d\n", -3, isPositive(-3));
     printf("%x\n", reverseBytes(0x01020304));
     printf("%d\n", isPower2(5));
     printf("%d\n", isPower2(8));
@@ -74,7 +77,15 @@ int negate(int x)
 
 int isPositive(int x)
 {
-    return (((~x) >> 31) & 1) & (x);
+    //  CASO    || signo_x | es_cero | !signo_x | !es_cero | (!signo_x) & (!es_cero)
+    // -----------------------------------------------------------------------------
+    // positivo ||    0    |    0    |     1    |     1    |           1
+    // cero     ||    0    |    1    |     1    |     0    |           0
+    // negativo ||    1    |    0    |     0    |     1    |           0
+    //
+    int signo_x = (x >> 31) & 1;
+    int es_cero = !x;
+    return (!signo_x) & (!es_cero);
 }
 
 int reverseBytes(int x)
@@ -83,16 +94,18 @@ int reverseBytes(int x)
     int b1 = (x >> 8) & 0xFF;
     int b2 = (x >> 16) & 0xFF;
     int b3 = (x >> 24) & 0xFF;
-    // printf("%x\n", b0);
-    // printf("%x\n", b1);
-    // printf("%x\n", b2);
-    // printf("%x\n", b3);
     return (b0 << 24) | (b1 << 16) | (b2 << 8) | (b3);
 }
 
 int isGreater(int x, int y)
 {
+    int menos_y = (~y) + 1;
+    int diferencia = x + menos_y;
 
+    int signo_diferencia = (diferencia >> 31) & 1;
+    int es_cero_la_diferencia = !diferencia;
+
+    return (!signo_diferencia) & (!es_cero_la_diferencia);
 }
 
 int isPower2(int x)
